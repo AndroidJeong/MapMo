9c8627171cc2f1919f4099521d4c92efbe1e611c
jeong-gyeongwoo <okbamtol@naver.com>
2024-10-27 18:29:03 +0900

    first commit

---
+++ app/src/main/java/com/jeong/mapmo/MapmoApplication.kt
@@ -0,0 +1,4 @@
+package com.jeong.mapmo
+
+class MapmoApplication {
+}
---
+++ app/src/main/java/com/jeong/mapmo/util/Constants.kt
@@ -0,0 +1,7 @@
+package com.jeong.mapmo.util
+
+object Constants {
+    const val BASE_URL = ""
+    const val API_KEY = ""
+
+}
