d75a548e51b8631b8632548d8f0ed4589bf72bb6
최현정 <hjchoi.dev@gmail.com>
2024-11-04 12:45:33 +0900

    design: Update Onboarding Button Layout

--- app/src/main/java/com/jeong/mapmo/ui/view/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/jeong/mapmo/ui/view/onboarding/OnboardingViewModel.kt
@@ -19,21 +19,42 @@ class OnboardingViewModel(private val resourceRepository: ResourceRepository) : 
                 isSkip = true
             ),
             OnboardingItem(
-                title = "권한 필요2",
+                title = "정확한 위치",
                 image = R.drawable.ic_launcher_background,
                 description = getString(R.string.onboarding_01),
                 buttonText = getString(R.string.onboarding_next),
                 isSkip = true
             ),
             OnboardingItem(
-                title = "권한 필요3",
+                title = "위치 액세스 권한\n항상 허용",
                 image = R.drawable.ic_launcher_background,
                 description = getString(R.string.onboarding_01),
                 buttonText = getString(R.string.onboarding_next),
                 isSkip = true
             ),
             OnboardingItem(
-                title = "권한 필요4",
+                title = "내 신체 활동 정보",
+                image = R.drawable.ic_launcher_background,
+                description = getString(R.string.onboarding_01),
+                buttonText = getString(R.string.onboarding_next),
+                isSkip = true
+            ),
+            OnboardingItem(
+                title = "알람 및 리마인더\n권한 허용",
+                image = R.drawable.ic_launcher_background,
+                description = getString(R.string.onboarding_01),
+                buttonText = getString(R.string.onboarding_next),
+                isSkip = true
+            ),
+            OnboardingItem(
+                title = "배터리 사용량 최적화 중지\n(추천)",
+                image = R.drawable.ic_launcher_background,
+                description = getString(R.string.onboarding_01),
+                buttonText = getString(R.string.onboarding_next),
+                isSkip = true
+            ),
+            OnboardingItem(
+                title = "권한 설정 완료!",
                 image = R.drawable.ic_launcher_background,
                 description = getString(R.string.onboarding_01),
                 buttonText = getString(R.string.onboarding_next),
