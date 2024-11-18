7f0b1145d524a2d846d4f10fab5e3d0554a61b4c
최현정 <hjchoi.dev@gmail.com>
2024-11-18 09:00:45 +0900

    chore: 코드 정리

--- app/src/main/java/com/jeong/mapmo/data/dto/Memo.kt
+++ app/src/main/java/com/jeong/mapmo/data/dto/Memo.kt
@@ -7,13 +7,13 @@ import kotlinx.parcelize.Parcelize
 //수정 상수 수정하기
 @Parcelize
 data class Memo(
-    var title:String,
+    var title: String,
     var location: String,
-    val longitude:Double = 0.0,
-    val latitude:Double = 0.0,
+    val longitude: Double = 0.0,
+    val latitude: Double = 0.0,
     var detail: String,
     var priority: PriorityColor = PriorityColor.RED,
     var checked: Boolean = false,
     var expand: Boolean = false,
     var isClamped: Boolean = false
-): Parcelable
+) : Parcelable
